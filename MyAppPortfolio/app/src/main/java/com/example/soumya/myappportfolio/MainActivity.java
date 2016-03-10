3441977dffb3a56f6209e032880d49a179ea8a20
soummyaah <soumya13106@iiitd.ac.in>
2016-03-11 01:50:07 +0530

    Project0

---
+++ MyAppPortfolio/app/src/main/java/com/example/soumya/myappportfolio/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.soumya.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listView = (ListView) findViewById(R.id.myAppsList);
+        String[] values = new String[] {
+                "Spotify Steamer",
+                "Scores App",
+                "Library App",
+                "Build It Bigger",
+                "XYZ Reader",
+                "Capstone: My Own App",
+        };
+        ArrayList<String> appNameList = new ArrayList<String>();
+        appNameList.addAll(Arrays.asList(values));
+
+        SpecialAdapter listAdapter = new SpecialAdapter(this,
+                R.layout.my_app_list_item, R.id.textListItemMyApp, appNameList);
+
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(position);
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "This button will launch my " + itemValue + "app", Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    public class SpecialAdapter extends ArrayAdapter{
+
+        private int[] colors = new int[] { 0xFF8B8B8B, 0xFFFF6F00 };
+
+        public SpecialAdapter(Context context, int resource, int textViewResourceId, ArrayList<String> objects) {
+            super(context, resource, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            if(position == 5) {
+                view.setBackgroundColor(colors[0]);
+            } else {
+                view.setBackgroundColor(colors[1]);
+            }
+            return view;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
